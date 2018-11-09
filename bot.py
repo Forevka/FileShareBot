f809ed5c9a1b329ae9e107e4ba98f6d68aba62a9
Forevka <starfield@i.ua>
2018-11-09 16:59:02 +0200

    added state machine, inline mode, chat search mode and etc

--- bot.py
+++ bot.py
@@ -5,8 +5,13 @@ import db
 import ssl
 import sys
 import json
+from typing import Optional
 from aiogram.utils.exceptions import BotBlocked
 from datetime import datetime
+
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from aiohttp import web
 from aiogram import Bot, Dispatcher, executor, types
@@ -29,37 +34,72 @@ WEBAPP_PORT = 443
 
 # Configure logging
 logging.basicConfig(filename="log.log", level=logging.WARNING)
-
+#logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 loop = asyncio.get_event_loop()
 
-#print(dir(types.ContentType))
+greet_kb = ReplyKeyboardMarkup(resize_keyboard = True)
+greet_kb.add(KeyboardButton('🗄 Меню 🗄'))
+greet_kb.add(KeyboardButton('❓ Как пользоваться? ❓'))
+greet_kb.add(KeyboardButton('🔎 Найти файл 🔎'))
 
+storage = MemoryStorage()
 bot = Bot(token=API_TOKEN, loop=loop)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+
+class Form(StatesGroup):
+    file_name = State()  # Will be represented in storage as 'Form:name'
+
+def extract_unique_code(text):
+    return text.split()[1] if len(text.split()) > 1 else None
+
+async def send_start_message(message):
+    await message.reply("Привет, я бот который поможет сохранить твои файлы", reply_markup=greet_kb)
+
+@dp.message_handler(commands=['start'])
+async def send_start(message: types.Message):
+    if message.chat.type == "private":
+        chat_id = message.chat.id
+        file_id = extract_unique_code(message.text);
+        if file_id:
+            file = await db.find_file_by_id(file_id);
+            if file!=None:
+                await bot.send_document(chat_id, file['file_id'], parse_mode = "HTML", reply_markup=greet_kb,reply_to_message_id = message.message_id,
+                                    caption='Привет! Вот твой файл\n\n<b>Файл:</b> '+file['file_name']+"\n<b>Дата загрузки:</b> "+datetime.strftime(file['create_date'], "%Y.%m.%d %H:%M:%S"))
+            else:
+                await message.reply("Не могу понять что за файл ты хочешь... Попробуй еще раз", reply_markup=greet_kb)
+        else:
+            await send_start_message(message)
 
 @dp.message_handler(commands=['find'])
-async def send_add(message: types.Message):
+async def send_find(message: types.Message):
     file_name = message.text.split(" ", 1)[-1]
-    #print(file_name)
-    if len(file_name)>=3:
-        files = await loop.create_task(db.find_file_by_name(file_name));
-        if len(files)>0:
-            files_kb = InlineKeyboardMarkup()
-            for i in files:
-                files_kb.add(InlineKeyboardButton(i['file_name'], callback_data=i['file_id']))
-            await message.reply("Вот что нашел по твоему запросу", reply_markup=files_kb)
+    print(file_name)
+    if file_name!="/find":
+        if len(file_name)>=3:
+            files = await loop.create_task(db.find_file_by_name(file_name));
+            if len(files)>0:
+                files_kb = InlineKeyboardMarkup()
+                for i in files:
+                    files_kb.add(InlineKeyboardButton(i['file_name'], callback_data=i['file_id']))
+                await message.reply("Вот что нашел по твоему запросу", reply_markup=files_kb)
+            else:
+                await message.reply("Ничего не нашел :(")
         else:
-            await message.reply("Ничего не нашел :(")
+            await message.reply("Минимум 3 буквы для поиска")
     else:
-        await message.reply("Минимум 3 буквы для поиска")
+        await message.reply("Чтобы воспользоваться командой напиши /find имя_файла")
+
+@dp.message_handler(content_types=types.ContentType.AUDIO)
+async def get_music(message: types.Message):
+    print(message)
+    err = await loop.create_task(db.insert_file_id(message.document.file_name, message.document.file_id, message.from_user.id))
+    if err == 1:
+        await message.reply("Окей, сохранил твой файл к себе в базу данных!")
 
 @dp.message_handler(content_types=types.ContentType.DOCUMENT)
-async def send_add(message: types.Message):
-    #print(message)
-    
-    await loop.create_task(db.insert_file_id(message.document.file_name, message.document.file_id, message.from_user.id))
-    await message.reply("Окей, сохранил твой файл к себе в базу данных!")
+async def get_document(message: types.Message):
+    await message.reply("Пока что не поддерживаю музыку но скоро буду 😉")
 
 @dp.callback_query_handler()
 async def process_callback(callback_query: types.CallbackQuery):
@@ -73,22 +113,71 @@ async def process_callback(callback_query: types.CallbackQuery):
         await bot.answer_callback_query(callback_query.id, text = "Отправил файл тебе в лс")
     except BotBlocked:
         await bot.answer_callback_query(callback_query.id, text = "Не могу отправить тебе файл, напиши мне для начала чтоб я мог отправлять тебе файлы", show_alert = True)
-        
 
-#@dp.message_handler()
-#async def echo(message: types.Message):
-#    await bot.send_message(message.chat.id, message.text)
-    
+@dp.inline_handler()
+async def inline_send(inline_query: types.InlineQuery):
+    if len(inline_query.query)>=3:
+        files = await db.find_file_by_name(inline_query.query);
+        if len(files)>0:
+            result = [];
+            for n, file in enumerate(files):
+                item = types.InlineQueryResultDocument(id = str(n), title = file['file_name'],
+                                                        document_url = file['file_id'], mime_type = "application")
+                result.append(item)
+            await bot.answer_inline_query(inline_query.id, results=result, cache_time=3)
+        else:
+            input_content = types.InputTextMessageContent("Немогу найти такого файла 🙄")
+            item = types.InlineQueryResultArticle(id='1', title='Немогу найти такого файла 🙄',
+                                                  input_message_content=input_content)
+            await bot.answer_inline_query(inline_query.id, results=[item], cache_time=60*1)
+    else:
+        input_content = types.InputTextMessageContent("Введи первые 3 буквы для поиска файла")
+        item = types.InlineQueryResultArticle(id='1', title='Введи первые 3 буквы для поиска файла',
+                                                  input_message_content=input_content)
+        await bot.answer_inline_query(inline_query.id, results=[item], cache_time=60*1)
+
+@dp.message_handler(lambda message: message.text == '🗄 Меню 🗄')
+async def send_menu(message: types.Message):
+    await send_start_message(message);
+
+@dp.message_handler(lambda message: message.text == '❓ Как пользоваться? ❓')
+async def send_how_to(message: types.Message):
+    await message.reply("Отправь мне файл чтобы я смог его сохранить.\n\nПоиск по всем файлам - /find имя_файла\nТакже ты можешь искать файлы просто набрав в чате мое имя @StoreMyFileBot\nИли просто нажми кнопку <b>🔎 Найти файл 🔎</b>", parse_mode = "HTML")
+
+@dp.message_handler(lambda message: message.text == '🔎 Найти файл 🔎')
+async def send_search(message: types.Message):
+    await Form.file_name.set()
+    await message.reply("Введи название файла или /cancel для отмены")
+
+@dp.message_handler(state=Form.file_name)
+async def process_file_name(message: types.Message, state: FSMContext):
+    """
+    Process file name
+    """
+    if message.text!="/cancel":
+        files = await loop.create_task(db.find_file_by_name(message.text));
+        print(files)
+        if len(files)>0:
+            files_kb = InlineKeyboardMarkup()
+            for i in files:
+                files_kb.add(InlineKeyboardButton(i['file_name'], callback_data=i['file_id']))
+            await message.reply("Вот что нашел по твоему запросу", reply_markup=files_kb)
+        else:
+            await message.reply("Ничего не нашел :(")
+        await state.finish()
+    else:
+        await message.reply("Отмена поиска.")
+        await state.finish()
+
 async def on_startup(dp):
     await bot.set_webhook(WEBHOOK_URL+WEBHOOK_PATH, certificate=open("webhook_cert.pem", 'rb'))
-    # insert code here to run it after startUU
 
 async def on_shutdown(dp):
     # insert code here to run it before shutdown
     pass
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp)
     #########
     #context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
     #context.load_cert_chain("webhook_cert.pem", "webhook_pkey.pem")
--- db.py
+++ db.py
@@ -3,25 +3,46 @@ import asyncpg
 
 async def insert_file_id(file_name, file_id, owner_id):
     conn = await asyncpg.connect(user='postgres', password='werdwerd',
-                                 database='files', host='194.67.205.185')
-    values = await conn.fetch('''INSERT INTO all_files(file_name, file_id, owner_id, create_date) VALUES('{}', '{}', {}, current_timestamp)'''.format(file_name, file_id, owner_id))
+                                 database='files_new', host='194.67.205.185')
+    values = await conn.execute("""INSERT INTO all_files(file_name, file_id, owner_id, create_date) VALUES('{}', '{}', {}, current_timestamp) ON CONFLICT (file_id) DO NOTHING RETURNING 0;""".format(file_name.replace("'","''"), file_id, owner_id))
     await conn.close()
+    if values.split(" ")[-1]=='0':
+        return 0; #nothing added
+    else:
+        return 1; #added new file id
+    
 
 async def find_file_by_name(file_name, offset = 0):
     conn = await asyncpg.connect(user='postgres', password='werdwerd',
-                                 database='files', host='194.67.205.185')
-    values = await conn.fetch("SELECT file_name, file_id, owner_id, create_date FROM all_files WHERE LOWER(file_name) LIKE LOWER('{}%') LIMIT 5 OFFSET {}".format(file_name, offset))
+                                 database='files_new', host='194.67.205.185')
+    values = await conn.fetch("SELECT file_name, file_id, owner_id, create_date FROM all_files WHERE LOWER(file_name) LIKE LOWER('{}%') LIMIT 5 OFFSET {}".format(file_name.replace("'","''"), offset))
     await conn.close()
     return values;
 
 async def find_file_by_id(file_id, offset = 0):
     conn = await asyncpg.connect(user='postgres', password='werdwerd',
-                                 database='files', host='194.67.205.185')
+                                 database='files_new', host='194.67.205.185')
     values = await conn.fetch("SELECT file_name, file_id, owner_id, create_date FROM all_files WHERE file_id = '{}' LIMIT 5 OFFSET {}".format(file_id, offset))
     await conn.close()
-    #print(values);
-    return values[0];
+    if len(values)>0:
+        return values[0];
+    else:
+        return None;
 
 
+async def insert_file_all(file_name, file_id, owner_id, timestamp):
+    conn = await asyncpg.connect(user='postgres', password='werdwerd',
+                                 database='files_new', host='194.67.205.185')
+    values = await conn.execute('''INSERT INTO all_files(file_name, file_id, owner_id, create_date) VALUES('{}', '{}', {}, '{}')'''.format(file_name, file_id, owner_id, timestamp))
+    await conn.close()
+
+'''
+async def f():
+    for i in open("data.txt", "r"):
+        i = i.split("	")
+        print(i);
+        await loop.create_task(insert_file_all(i[0], i[1], i[2], i[3]))
+'''
 #loop = asyncio.get_event_loop()
-#loop.run_until_complete(find_file_by_id("BQADAgADYQEAAjDFGEnsf5zeIaXIJQI"))
+#loop.run_until_complete(f())
+
